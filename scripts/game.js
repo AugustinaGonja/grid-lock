ec14b0bb77a76ad5a706902db140e660753a6fff
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-04-16 16:11:17 +0100

    Change variable name.

--- scripts/game.js
+++ scripts/game.js
@@ -13,12 +13,12 @@ function generatePattern() {
   for (i = 0; i < boxes.length; i++) {
     // Add all with box class to the allBoxes array
     allBoxes.push(i)
-}
+};
 // Generate an array which has a length of 8 random numbers from the allBoxes array and add to pattern.
-while (pattern.length < chosenBoxes) {
+while (pattern.length < boxesToRecall) {
     let randNum = Math.floor(Math.random() * boxes.length);
     pattern.push(randNum);
-}
+};
 console.log(boxes);
 console.log(allBoxes);
 console.log(pattern);
