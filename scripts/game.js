37d9e7512f67b5432f7e5589f859b240c76db8db
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-04-16 17:04:36 +0100

    Add if statement to generatePattern( ), to prevent repeats in pattern.

--- scripts/game.js
+++ scripts/game.js
@@ -13,15 +13,20 @@ function generatePattern() {
   for (i = 0; i < boxes.length; i++) {
     // Add all with box class to the allBoxes array
     allBoxes.push(i)
-};
-// Generate an array which has a length of 8 random numbers from the allBoxes array and add to pattern.
-while (pattern.length < boxesToRecall) {
+  };
+  // Generate an array which has a length of 8 random numbers from the allBoxes array and add to pattern.
+  while (pattern.length < boxesToRecall) {
     let randNum = Math.floor(Math.random() * boxes.length);
     pattern.push(randNum);
-};
-console.log(boxes);
-console.log(allBoxes);
-console.log(pattern);
+  };
+  /* Ensure that no numbers are repeats.
+  If the pattern includes randNum already , dont include in pattern*/
+  if (!pattern.includes(randNum)) {
+    pattern.push(randNum);
+  };
+  console.log(boxes);
+  console.log(allBoxes);
+  console.log(pattern);
 }
 
 generatePattern();
