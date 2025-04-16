0307821b064920d925277f37af35ed5e5f2e4682
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-04-16 15:58:39 +0100

    Define variables. Use queryselector to access all items of box class.

---
+++ scripts/game.js
@@ -0,0 +1,6 @@
+// GridLock
+let boxes = document.querySelectorAll(".box");
+let scoreCounter = document.getElementById("correct-tiles");
+let restartGame = document.getElementById("restart");
+const chosenBoxes = 8;
+
