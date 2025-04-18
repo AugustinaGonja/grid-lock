de04ce4f8eb5cc90867eee54bcf5773a471981b7
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-04-18 15:20:17 +0100

    Rename variable for the restart ID . Previoius name will be use for function name - bug fix.

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,7 @@
 // GridLock
 let boxes = document.querySelectorAll(".box");
 let scoreCounter = document.getElementById("correct-tiles");
-let restartGame = document.getElementById("restart");
+let restartButton = document.getElementById("restart");
 
 const boxesToRecall = 8;
 let pattern = [];
@@ -20,42 +20,39 @@ function generatePattern() {
   while (pattern.length < boxesToRecall) {
     let randNum = Math.floor(Math.random() * boxes.length);
     //pattern.push(randNum);
-  /* Ensure that no numbers are repeats.
-  If the pattern includes randNum already , dont include in pattern*/
-  if (!pattern.includes(randNum)) {
-    pattern.push(randNum);
+    /* Ensure that no numbers are repeats.
+    If the pattern includes randNum already , dont include in pattern*/
+    if (!pattern.includes(randNum)) {
+      pattern.push(randNum);
+    };
   };
-};
   console.log(boxes);
   console.log(allBoxes);
   console.log(pattern);
-}
-generatePattern();
+};
 
 
 // Reveal Pattern 
 
-function revealPattern(){
+function revealPattern() {
   let revealTime = 10000 // Reveal time is 10s
 
-  for (let i = 0 ; i < pattern.length ; i++) {
+  for (let i = 0; i < pattern.length; i++) {
     boxes[pattern[i]].style.backgroundColor = "#ed9121";
   }
-// Will ensure that the pattern disappears within the time limit 
-setTimeout(function () {
-  for (let i = 0; i < pattern.length; i++) {
-    boxes[pattern[i]].style.backgroundColor = "";
-    // Restores color back to normal for all highlighted tiles
-  }
-}, revealTime);
+  // Will ensure that the pattern disappears within the time limit 
+  setTimeout(function () {
+    for (let i = 0; i < pattern.length; i++) {
+      boxes[pattern[i]].style.backgroundColor = "";
+      // Restores color back to normal for all highlighted tiles
+    }
+  }, revealTime);
 }
-
-revealPattern();
 
 // Click Tile
 
-function clickTile (){
-  for (let i = 0 ; i < boxes.length ; i++){
+function clickTile() {
+  for (let i = 0; i < boxes.length; i++) {
     boxes.addEventListener("click", userClick);
   }
 };
@@ -63,31 +60,38 @@ function clickTile (){
 // User Click
 // Takes care of what will happen when user clicks correct or incorrect tile
 
-function userClick(e){
+function userClick(e) {
   let selectedBox = e.target;
   let boxIndex = indexOf.selectedBox; // Get index of box that player has selected
 
-  if (pattern.includes(boxIndex)){
+  if (pattern.includes(boxIndex)) {
     if (!selectedBox.style.backgroundColor) // if box isnt already green ,then user can click and inc score if it is part of array
-    selectedBox.style.backgroundColor="#568203" //Change Colour to Green if player has selected box that is inc in array.
+      selectedBox.style.backgroundColor = "#568203" //Change Colour to Green if player has selected box that is inc in array.
     scoreCounter.textContent = correctTiles;
     correctTiles++;//increment score by 1
 
-    if (correctTiles === pattern.length){
+    if (correctTiles === pattern.length) {
       alert("Congratulations! You are a winner :)");
-    restartGame()}
+      restartGame()
+    }
 
- }else{
-  selectedBox.style.backgroundColor="#c21807"//Change Colour to Red if otherwise
-  alert("Oh no ! This is the Wrong tile :( Game Over.")
- }
+  } else {
+    selectedBox.style.backgroundColor = "#c21807"//Change Colour to Red if otherwise
+    alert("Oh no ! This is the Wrong tile :( Game Over.")
+  }
 };
 
-userClick(e);
 
 // Start/Restart Game
 
-function restartGame (){
+function restartGame() {
+  // Clear Score Count back to 0
+  scoreCounter.textContent = 0
+  // Call generatePattern and revealPattern
+  generatePattern();
+  revealPattern();
+};
 
-}
- 
+// Add Event Listener to Restart Game button  
+
+restartButton.addEventListener("click", restartGame);
